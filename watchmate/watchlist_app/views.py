911c01d8a9bb4c80c72b0db18fd701196af162b0
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2022-06-17 23:51:49 +0600

    drf start

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from watchlist_app.models import Moive
 from django.http import JsonResponse
 # Create your views here.
-def moivelist(request):
-    moives=Moive.objects.all()
-    data={'moives':list(moives.values())}
-    return JsonResponse(data  )
 
-def moive_details(request,pk):
-    moive=Moive.objects.get(pk=pk)
-    data={
-        'name':moive.name,
-        'active':moive.active,
-        'description':moive.description
-  
-        
+
+def moivelist(request):
+    moives = Moive.objects.all()
+    data = {'moives': list(moives.values())}
+    return JsonResponse(data)
+
+
+def moive_details(request, pk):
+    moive = Moive.objects.get(pk=pk)
+    data = {
+        'name': moive.name,
+        'active': moive.active,
+        'description': moive.description
+
+
     }
-    return JsonResponse(data  )
+    return JsonResponse(data)
+ 
